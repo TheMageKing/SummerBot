ac0d911e0341b8d7187a5fc5b5e4d873d0b7a675
Calum McConnell <calumlikesapplepie@gmail.com>
2021-06-05 15:22:33 -0400

    set up manual drive on button press

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.*;
+
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +19,20 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  
+  private DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem(); 
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
+  private Joystick joystickObj = new Joystick(0);
+  private JoystickButton buttonObj = new JoystickButton(joystickObj,1);
+  private JoystickButton buttonObj2 = new JoystickButton(joystickObj,2);
+  
+  private Command m_autoCommand;
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+    
+
   }
 
   /**
@@ -34,7 +41,16 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    buttonObj.whenActive(()->{
+      drivetrainSubsystem.arcadeDrive(
+        joystickObj.getX(), joystickObj.getY());
+    }, drivetrainSubsystem);
+    
+    buttonObj2.whenActive(() ->{
+      System.out.println(2);
+    });
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
