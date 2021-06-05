2a11464c00e5be03ab5aa7bf18ffaa8e6bdc43fd
Calum McConnell <calumlikesapplepie@gmail.com>
2021-06-05 14:40:11 -0400

    Allow driving using arcade drive

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -4,14 +4,18 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class DrivetrainSubsystem extends SubsystemBase {
-  private TalonFX frontRightFalconMotor = new TalonFX(-1);
-  private TalonFX frontLeftFalconMotor = new TalonFX(-7);
-  private TalonFX backRightFalconMotor = new TalonFX(-1);
-  private TalonFX backLeftFalconMotor = new TalonFX(-1);
+  private WPI_TalonFX frontRightFalconMotor = new WPI_TalonFX(-1);
+  private WPI_TalonFX frontLeftFalconMotor = new WPI_TalonFX(-7);
+  private WPI_TalonFX backRightFalconMotor = new WPI_TalonFX(-1);
+  private WPI_TalonFX backLeftFalconMotor = new WPI_TalonFX(-1);
+
+  private DifferentialDrive differentialDrive = 
+      new DifferentialDrive(frontLeftFalconMotor, frontRightFalconMotor);  
   
   /** Creates a new DrivetrainSubsystem. */
   public DrivetrainSubsystem() {
@@ -31,6 +35,15 @@ public class DrivetrainSubsystem extends SubsystemBase {
     backLeftFalconMotor.follow(frontLeftFalconMotor);
   }
   
+  /**
+   * Drive the robot using arcade drive
+   * Arcade drive does square the inputs. 
+   * @param speed how fast the robot moves
+   * @param rotation how the robot turns
+   */
+  public void arcadeDrive(double speed, double rotation){
+    differentialDrive.arcadeDrive(speed, rotation, true);
+  }
 
   @Override
   public void periodic() {
